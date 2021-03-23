b1287a86deaa3298795a34bce1fb83ddfaaeaa8c
sin00829 <sin0000829@naver.com>
2021-03-23 11:57:31 +0900

    sw

--- hello.js
+++ hello.js
@@ -3,4 +3,16 @@ console.log('hello world...!');
 //console.log('hello world...!');
 String("성원");
 console.log(5+3*2);
-
+let a=(new Date()).getHours();
+console.log(a<3 || a<8);
+console.log (a=10);
+console.log(a)
+a++ 
+a-- 
+typeof"문자열"
+'string'
+typeof 10
+'number'
+let c= true
+typeof(c)
+'boolean'
